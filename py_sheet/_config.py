94db4df221b7d9d456a65ca3cce380647cef09ac
Vaughn Haynes <153122882+lhaynes-ss@users.noreply.github.com>
2024-03-06 13:39:26 -0600

    update

--- py_sheet/_config.py
+++ py_sheet/_config.py
@@ -4,22 +4,40 @@ import os
 Note:
 - Sheet for excel needs to be named "weekly" or "monthly"
 - Tables within sheet need to be named "campaign", "download", "time". Do this from "design" tab
+
+Buckets:
+- samsung.ads.data.share
+- dev-samsung-dm-data-share-paramount-plus
+- prod-samsung-dm-data-share-paramount-plus
+- dev-samsung-dm-data-share-pluto
+- prod-samsung-dm-data-share-pluto
+
+* when udw_stage key is present, bucket and prefix will be modified with app_temp_storage value
 '''
 
 
 def get_app_config():
     return {
         'path': os.path.realpath(os.path.dirname(__file__)) + '\\'
+        ,'udw_stage_keys': {
+            'udw_test':         {'stage': '@adbiz_data.samsung_ads_data_share/analytics/custom/vaughn/test/python/udw/'} 
+            ,'udw_pplus_us':    {'stage': '@udw_marketing_analytics_reports.paramount_plus_external_us/'} 
+            ,'udw_pplus_int':   {'stage': '@udw_marketing_analytics_reports.paramount_plus_external_international/'} 
+            ,'udw_pluto_us':    {'stage': '@udw_marketing_analytics_reports.pluto_external_us/'} 
+            ,'udw_pluto_int':   {'stage': '@udw_marketing_analytics_reports.pluto_external_international/'} 
+        }
         ,'destination_bucket_keys': {
-            'test'              : {'bucket': 'samsung.ads.data.share', 'prefix': 'analytics/custom/vaughn/test/python/'}
-            ,'dev_pplus_us'     : {'bucket': 'dev-samsung-dm-data-share-paramount-plus', 'prefix': 'paramount-plus-us/'}
-            ,'dev_pplus_int'    : {'bucket': 'dev-samsung-dm-data-share-paramount-plus', 'prefix': 'paramount-plus-international/'}
-            ,'prod_pplus_us'    : {'bucket': 'prod-samsung-dm-data-share-paramount-plus', 'prefix': 'paramount-plus-us/'}
-            ,'prod_pplus_int'   : {'bucket': 'prod-samsung-dm-data-share-paramount-plus', 'prefix': 'paramount-plus-international/'}
-            ,'dev_pluto_us'     : {'bucket': 'dev-samsung-dm-data-share-pluto', 'prefix': 'pluto-us/'}
-            ,'dev_pluto_int'    : {'bucket': 'dev-samsung-dm-data-share-pluto', 'prefix': 'pluto-international/'}
-            ,'prod_pluto_us'    : {'bucket': 'prod-samsung-dm-data-share-pluto', 'prefix': 'pluto-us/'}
-            ,'prod_pluto_int'   : {'bucket': 'prod-samsung-dm-data-share-pluto', 'prefix': 'pluto-international/'}
+            'app_temp_storage': {'bucket': 'samsung.ads.data.share', 'prefix': 'analytics/custom/vaughn/temp_storage/', 'udw_stage': ''}
+            ,'dev_test':        {'bucket': 'samsung.ads.data.share', 'prefix': 'analytics/custom/vaughn/test/python/', 'udw_stage': ''}
+            ,'prod_test':       {'bucket': 'foo.bar', 'prefix': 'pplus_pluto/', 'udw_stage': 'udw_test'}
+            ,'dev_pplus_us':    {'bucket': 'dev-samsung-dm-data-share-paramount-plus', 'prefix': 'paramount_plus_us/', 'udw_stage': ''}
+            ,'dev_pplus_int':   {'bucket': 'dev-samsung-dm-data-share-paramount-plus', 'prefix': 'paramount-plus-international/', 'udw_stage': ''}
+            ,'prod_pplus_us':   {'bucket': 'prod-samsung-dm-data-share-paramount-plus', 'prefix': 'paramount_plus_us/', 'udw_stage': 'udw_pplus_us'}
+            ,'prod_pplus_int':  {'bucket': 'prod-samsung-dm-data-share-paramount-plus', 'prefix': 'paramount-plus-international/', 'udw_stage': 'udw_pplus_int'}
+            ,'dev_pluto_us':    {'bucket': 'dev-samsung-dm-data-share-pluto', 'prefix': 'pluto-us/', 'udw_stage': ''}
+            ,'dev_pluto_int':   {'bucket': 'dev-samsung-dm-data-share-pluto', 'prefix': 'pluto-international/', 'udw_stage': ''}
+            ,'prod_pluto_us':   {'bucket': 'prod-samsung-dm-data-share-pluto', 'prefix': 'pluto-us/', 'udw_stage': 'udw_pluto_us'}
+            ,'prod_pluto_int':  {'bucket': 'prod-samsung-dm-data-share-pluto', 'prefix': 'pluto-international/', 'udw_stage': 'udw_pluto_int'}
         }
     }
 
@@ -31,7 +49,7 @@ def get_report_config():
         ,'reports':[
             # {
             #     'partner': 'paramount_plus'                       # paramount_plus | pluto
-            #     ,'bucket_key': 'test'                             # see 'destination_bucket_keys' dictionary above for options
+            #     ,'bucket_key': 'dev_test'                             # see 'destination_bucket_keys' dictionary above for options
             #     ,'interval': 'weekly'                             # weekly | monthly
             #     ,'region': 'au'                                   # e.g., fr | es | de | it
             #     ,'filename': 'input\\test_files\\test_pplus_au.xlsx'     # file name or path relative to this directory
@@ -41,14 +59,14 @@ def get_report_config():
             # --------------------------------------
             {
                 'partner': 'paramount_plus'
-                ,'bucket_key': 'test' 
+                ,'bucket_key': 'prod_test' 
                 ,'interval': 'weekly'
                 ,'region': 'au'
                 ,'filename': 'input\\test_files\\test_pplus_au.xlsx'
             }
             ,{
                 'partner': 'paramount_plus'
-                ,'bucket_key': 'test' 
+                ,'bucket_key': 'prod_test' 
                 ,'interval': 'weekly'
                 ,'region': 'fr'
                 ,'filename': 'input\\test_files\\test_pplus_fr.xlsx'
@@ -58,14 +76,14 @@ def get_report_config():
             # --------------------------------------
             ,{
                 'partner': 'pluto'
-                ,'bucket_key': 'test' 
+                ,'bucket_key': 'prod_test' 
                 ,'interval': 'weekly'
                 ,'region': 'de'
                 ,'filename': 'input\\test_files\\test_pluto_de.xlsx'
             }
             ,{
                 'partner': 'pluto'
-                ,'bucket_key': 'test' 
+                ,'bucket_key': 'prod_test' 
                 ,'interval': 'weekly'
                 ,'region': 'fr'
                 ,'filename': 'input\\test_files\\test_pluto_fr.xlsx'

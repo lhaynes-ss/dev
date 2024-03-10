c760b09e56e028941a64f2dfb2fb84afcca59222
Vaughn Haynes <153122882+lhaynes-ss@users.noreply.github.com>
2024-03-10 16:46:48 -0500

    aws print

--- py_sheet/_config.py
+++ py_sheet/_config.py
@@ -21,22 +21,22 @@ def get_app_config():
         'path': os.path.realpath(os.path.dirname(__file__)) + '\\'
         ,'udw_stage_keys': {
             'udw_test':         {'stage': '@adbiz_data.samsung_ads_data_share/analytics/custom/vaughn/test/python/udw/'} 
-            ,'udw_pplus_us':    {'stage': '@udw_marketing_analytics_reports.paramount_plus_external_us/'} 
+            ,'udw_pplus_na':    {'stage': '@udw_marketing_analytics_reports.paramount_plus_external_us/'} 
             ,'udw_pplus_int':   {'stage': '@udw_marketing_analytics_reports.paramount_plus_external_international/'} 
-            ,'udw_pluto_us':    {'stage': '@udw_marketing_analytics_reports.pluto_external_us/'} 
+            ,'udw_pluto_na':    {'stage': '@udw_marketing_analytics_reports.pluto_external_us/'} 
             ,'udw_pluto_int':   {'stage': '@udw_marketing_analytics_reports.pluto_external_international/'} 
         }
         ,'destination_bucket_keys': {
             'app_temp_storage': {'bucket': 'samsung.ads.data.share', 'prefix': 'analytics/custom/vaughn/temp_storage/', 'udw_stage': ''}
             ,'dev_test':        {'bucket': 'samsung.ads.data.share', 'prefix': 'analytics/custom/vaughn/test/python/', 'udw_stage': ''}
             ,'prod_test':       {'bucket': 'foo.bar', 'prefix': 'pplus_pluto/', 'udw_stage': 'udw_test'}
-            ,'dev_pplus_us':    {'bucket': 'dev-samsung-dm-data-share-paramount-plus', 'prefix': 'paramount_plus_us/', 'udw_stage': ''}
+            ,'dev_pplus_na':    {'bucket': 'dev-samsung-dm-data-share-paramount-plus', 'prefix': 'paramount_plus_us/', 'udw_stage': ''}
             ,'dev_pplus_int':   {'bucket': 'dev-samsung-dm-data-share-paramount-plus', 'prefix': 'paramount-plus-international/', 'udw_stage': ''}
-            ,'prod_pplus_us':   {'bucket': 'prod-samsung-dm-data-share-paramount-plus', 'prefix': 'paramount_plus_us/', 'udw_stage': 'udw_pplus_us'}
+            ,'prod_pplus_na':   {'bucket': 'prod-samsung-dm-data-share-paramount-plus', 'prefix': 'paramount_plus_us/', 'udw_stage': 'udw_pplus_na'}
             ,'prod_pplus_int':  {'bucket': 'prod-samsung-dm-data-share-paramount-plus', 'prefix': 'paramount-plus-international/', 'udw_stage': 'udw_pplus_int'}
-            ,'dev_pluto_us':    {'bucket': 'dev-samsung-dm-data-share-pluto', 'prefix': 'pluto-us/', 'udw_stage': ''}
+            ,'dev_pluto_na':    {'bucket': 'dev-samsung-dm-data-share-pluto', 'prefix': 'pluto-us/', 'udw_stage': ''}
             ,'dev_pluto_int':   {'bucket': 'dev-samsung-dm-data-share-pluto', 'prefix': 'pluto-international/', 'udw_stage': ''}
-            ,'prod_pluto_us':   {'bucket': 'prod-samsung-dm-data-share-pluto', 'prefix': 'pluto-us/', 'udw_stage': 'udw_pluto_us'}
+            ,'prod_pluto_na':   {'bucket': 'prod-samsung-dm-data-share-pluto', 'prefix': 'pluto-us/', 'udw_stage': 'udw_pluto_na'}
             ,'prod_pluto_int':  {'bucket': 'prod-samsung-dm-data-share-pluto', 'prefix': 'pluto-international/', 'udw_stage': 'udw_pluto_int'}
         }
     }
@@ -59,15 +59,15 @@ def get_report_config():
             # --------------------------------------
             {
                 'partner': 'paramount_plus'
-                ,'bucket_key': 'prod_test' 
                 ,'interval': 'weekly'
+                ,'bucket_key': 'prod_test' 
                 ,'region': 'au'
                 ,'filename': 'input\\test_files\\test_pplus_au.xlsx'
             }
             ,{
                 'partner': 'paramount_plus'
-                ,'bucket_key': 'prod_test' 
                 ,'interval': 'weekly'
+                ,'bucket_key': 'prod_test' 
                 ,'region': 'fr'
                 ,'filename': 'input\\test_files\\test_pplus_fr.xlsx'
             }  
@@ -76,15 +76,15 @@ def get_report_config():
             # --------------------------------------
             ,{
                 'partner': 'pluto'
-                ,'bucket_key': 'prod_test' 
                 ,'interval': 'weekly'
+                ,'bucket_key': 'prod_test' 
                 ,'region': 'de'
                 ,'filename': 'input\\test_files\\test_pluto_de.xlsx'
             }
             ,{
                 'partner': 'pluto'
-                ,'bucket_key': 'prod_test' 
                 ,'interval': 'weekly'
+                ,'bucket_key': 'prod_test'
                 ,'region': 'fr'
                 ,'filename': 'input\\test_files\\test_pluto_fr.xlsx'
             }
---
+++ py_sheet/_print_aws.py
@@ -0,0 +1,52 @@
+'''
+===================================
+Print AWS
+===================================
+'''
+# dependencies
+import json
+import _config as cfg
+
+
+print('Starting job...\n')
+
+# get configs
+app_config  = cfg.get_app_config()
+path        = app_config['path']
+
+udw_dict    = {}
+
+
+# get json file
+with open(f"{path}output\\upload_list.json") as input_file:
+    file_dict = json.load(input_file)
+    
+    # print(file_dict)
+
+
+
+    # get files
+    for f_name, f_dictionary in file_dict.items():
+
+        f_path      = f_dictionary['output_file_path']
+        s3_bucket   = f_dictionary['s3_bucket']
+        s3_prefix   = f_dictionary['s3_prefix']
+        interval    = f_dictionary['interval']
+        stage       = f_dictionary['stage']
+        cols        = f_dictionary['cols']
+
+        # build a list of "copy to" and "copy from" paths for UDW
+        if stage != '':
+            udw_dict[f_name] = {
+                'to': f"{stage}{interval}/{f_name}" 
+                ,'from': f"s3://{s3_bucket}/{s3_prefix}{interval}/{f_name}"
+                ,'cols': cols
+             }
+
+
+        print(f"aws --profile nyc s3 ls s3://{s3_bucket}/{s3_prefix}{interval}/{f_name}")
+
+
+print('Done!')
+
+
